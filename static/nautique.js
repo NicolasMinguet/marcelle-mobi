ab4d68c86b21e4d5a8132bfac097c9b02772983b
Steeve <fitoussi.steeve@gmail.com>
2019-10-09 10:22:31 +0200

    nautique js

--- static/nautique.js
+++ static/nautique.js
@@ -24,5 +24,21 @@ const nautiques =
       "name": "Sojet 13",
       "description": "Magasin d'articles de ski nautique",
     },
+    {
+      "coordinates": {
+        "latitude": "43.242870",
+        "longitude": "5.375500",
+      },
+      "name": "Wind And Sea Association",
+      "description": "Club de sport",
+    },
+    {
+      "coordinates": {
+        "latitude": "43.292670",
+        "longitude": "5.367790",
+      },
+      "name": "Société Nautique de Marseille",
+      "description": "La Société nautique de Marseille est un club nautique privé, fondé en 1887 à Marseille. La Société nautique organise des régates et manifestations nautiques, s'occupe de la gestion de places dans le Vieux-Port, d'une école de voile, d'un pôle de voiliers traditionnels et d'un pôle course.",
+    }
   ]
 export default nautiques;
